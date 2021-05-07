40c9efa677ba728e95a2591260b2e3a11004c2ae
M3n4p <55096723+M3n4p@users.noreply.github.com>
2021-05-07 21:44:34 +0200

    Implemented NodeSquare/Start of Line Computation

--- Marching Squares/Assets/Scripts/LookUpGenerator.cs
+++ Marching Squares/Assets/Scripts/LookUpGenerator.cs
@@ -24,12 +24,40 @@ public class LookUpGenerator : MonoBehaviour
         }
     }
 
+    public class NodeSquare
+    {
+        Node upperLeft, upperRight, lowerLeft, lowerRight;
+        Vector3 midLeft, midDown, midRight, midUp;
+
+        public NodeSquare(Node uLeft, Node uRight, Node lLeft, Node lRight)
+        {
+            upperLeft = uLeft;
+            upperRight = uRight;
+            lowerLeft = lLeft;
+            lowerRight = lRight;
+
+            midLeft = CalcMidPoint(upperLeft, lowerLeft);
+            midDown = CalcMidPoint(lowerLeft, lowerRight);
+            midRight = CalcMidPoint(upperRight, lowerRight);
+            midUp = CalcMidPoint(upperLeft, upperRight);
+        }
+
+        private Vector3 CalcMidPoint(Node n1, Node n2)
+        {
+            return Vector3.Lerp(n1.position, n2.position, 0.5f);
+        }
+
+        private Vector3 CalcMidPoint(Node n1, Node n2, float iso)
+        {
+            return Vector3.Lerp(n1.position, n2.position, iso);
+        }
+    }
+
     public Toggle upperLeftToggle, upperRightToggle, lowerLeftToggle, lowerRightToggle;
     private Node upperLeft, upperRight, lowerLeft, lowerRight;
     public Text caseText;
 
-    private Vector3 startVertex, mousePos;
-    public Material mat;
+    public new Camera camera;
 
     private int caseNumber;
 
@@ -47,6 +75,24 @@ public class LookUpGenerator : MonoBehaviour
         lowerRightToggle.onValueChanged.AddListener(delegate { ChangeNodeActivation(lowerRightToggle, lowerRight); });
 
         caseNumber = 0;
+
+        Vector3 start = new Vector3(0, 2, 0);
+        Vector3 end = new Vector3(5, 0, 0);
+        //float thickness = 2.0f;
+
+        Vector3 directionLine = end - start;
+        Vector3 directionCamera = camera.transform.position - start;
+
+        Vector3 cross = Vector3.Cross(directionLine, directionCamera);
+
+        Vector3[] vertices = { start, cross, start, end };
+        int[] indices = { 0, 1, 2, 3 };
+
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertices;
+        mesh.SetIndices(indices, MeshTopology.Lines, 0);
+
+        GetComponent<MeshFilter>().mesh = mesh;
     }
 
     private void ChangeNodeActivation(Toggle change, Node n)
